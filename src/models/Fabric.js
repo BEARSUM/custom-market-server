83ad9680834ecb89a3a8a01202453b8031ef4ef5
SUMINMOON <sum95771@gmail.com>
2024-08-04 22:39:27 +0900

    feat: Fabric schema 및 model 생성

---
+++ src/models/Fabric.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const fabricSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Fabric", fabricSchema);
